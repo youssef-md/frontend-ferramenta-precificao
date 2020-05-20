b886fdba24a868f58ce6e1c1862f0f4fafc33a10
Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>
2020-05-20 10:43:45 -0300

    Open a new window with the requested html

--- src/App.js
+++ src/App.js
@@ -5,14 +5,8 @@ import { useState } from 'react';
 
 function App() {
 
-  const [html, setHtml] = useState('') 
 
   useEffect(() => {
-    // async function getSeila() {
-    //   const res = await axios.get('https://sso.staging.acesso.gov.br/authorize')
-    //   console.log(res.data)
-    // }
-    // getSeila()
     // https://auth-sso.dev-ecacpgfn.estaleiro.serpro.gov.br/api/login
 
     // ?response_type=code
@@ -23,19 +17,25 @@ function App() {
 
     // https://cors-anywhere.herokuapp.com/
     
-    // axios.get('https://cors-anywhere.herokuapp.com/https://auth-sso.dev-ecacpgfn.estaleiro.serpro.gov.br/api/login?response_type=code&scope=openid+email+phone+profile&redirect_uri=http%3A%2F%2Fappcliente.com.br%2Fphpcliente%2Floginecidadao.Php&nonce=3ed8657fd74c&state=358578ce6728b').then(data => {
-    //   console.log(data)
-    // })
+    //  Denatran
+    // https://sso.acesso.gov.br/authorize?response_type=code&client_id=portalservicos.denatran.serpro.gov.br&scope=openid+profile+email+govbr_empresa&redirect_uri=https%3A%2F%2Fportalservicos.denatran.serpro.gov.br%2FloginRetorno.html&nonce=36d4c7e2f346e&state=3e34adde59973
 
-    axios.get('https://sso.staging.acesso.gov.br/authorize?response_type=code&client_id=ec4318d6-f797-4d65-b4f7-39a33bf4d544&scope=openid+email+phone+profile&redirect_uri=http%3A%2F%2Fappcliente.com.br%2Fphpcliente%2Floginecidadao.Php&nonce=3ed8657fd74c&state=358578ce6728b').then(res => {
-      console.log(res.data)
-      setHtml(res.data)
-    })
+    // axios.get('https://sso.staging.acesso.gov.br/authorize?response_type=code&client_id=ec4318d6-f797-4d65-b4f7-39a33bf4d544&scope=openid+email+phone+profile&redirect_uri=http%3A%2F%2Fappcliente.com.br%2Fphpcliente%2Floginecidadao.Php&nonce=3ed8657fd74c&state=358578ce6728b').then(res => {
+      // console.log(res.data)
+    // })
   }, [])
 
+  function openNewWindow() {
+    
+    axios.get('https://cors-anywhere.herokuapp.com/https://www.google.com')
+    .then(res => {
+      const govbrPopup = window.open('', 'Título', 'height=500, width=500');
+      govbrPopup.document.write(res.data)
+    })
+  }
   
 
-  return <html dangerouslySetInnerHTML={{ __html: html}}/>
+  return <button onClick={openNewWindow}>Entrar com gov.br</button>
 }
 
 export default App;
