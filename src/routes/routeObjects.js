af4f5135bb4dcacb7ecc18deb588ab592c02dbc2
Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>
2020-07-15 11:10:24 -0300

    Fix breadcrumb routing problem

--- src/routes/routeObjects.js
+++ src/routes/routeObjects.js
@@ -1,21 +1,21 @@
 export const LISTAR_SERVICOS = {
   name: 'Listagem de Serviços',
-  route: 'listar-servicos',
+  route: '/listar-servicos',
 };
 
 export function SERVICO_SELECIONADO(name, id) {
   return {
     name,
-    route: `servico-selecionado/${id}`,
+    route: `/servico-selecionado/${id}`,
   };
 }
 
 export const PACOTE_CONSTANTES = {
   name: 'Pacote de constantes',
-  route: 'pacote-constantes',
+  route: '/pacote-constantes',
 };
 
 export const PREENCHER_MODELO = {
   name: 'Preencher modelo',
-  route: 'preencher-modelo',
+  route: '/preencher-modelo',
 };
